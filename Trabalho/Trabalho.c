f23c9f38f79a960792438fa4e127e0bc6ded02b5
LarissaVitto <larissareginacosta@gmail.com>
2024-08-26 20:55:06 -0300

    Terminado o trabalho

--- Trabalho/Trabalho.c
+++ Trabalho/Trabalho.c
@@ -7,16 +7,12 @@ double POTENCIACAO(double a, double b);
 double RAIZ(double a);
 int FATORIAL(int a);
 int MDC(int a, int b);
-/*int MMC(int a, int b);
-double EQUACAO(double a, double b);
-*/
-
-
-
+int MMC(int a, int b);
+double EQUACAO(double a, double b, double c);
 
 int main() {
     int contador = 0,A = 0, B = 0;
-    double a = 0.0, b = 0.0;
+    double a = 0.0, b = 0.0, c = 0.0;
 
     do{
          printf("Escolha uma operacao:\n0 - Sair\n1 - Soma\n2 - Subtracao\n3 - Multiplicacao\n4 - Divisao\n5 - Potenciacao\n6 - Raiz Quadrada\n7 - Fatorial\n8 - MDC\n9 - MMC\n10 - Equacao de Segundo Grau\nDigite a sua escolha: ");
@@ -51,7 +47,11 @@ int main() {
             scanf("%lf", &a);
             printf("Digite o segundo numero: ");
             scanf("%lf", &b);
-            printf("Resultado: %.5lf\n",DIVISAO(a, b));
+            if( a == 0|| b == 0){
+                printf("Resultado indefinido");
+            }else{
+                printf("Resultado: %.5lf\n",DIVISAO(a, b));
+            }
         }
         if (contador == 5)
         {   
@@ -88,8 +88,7 @@ int main() {
                 printf("Resultado: %d\n", FATORIAL(A));   
             }else{
                 printf("Resultado: 1\n");
-            }
-            
+            }        
         }  
         if (contador == 8)
         {   
@@ -105,7 +104,7 @@ int main() {
             scanf("%d", &A);
             printf("Digite o segundo numero: ");
             scanf("%d", &B);
-           // printf("Resultado: %d\n",MMC(A, B));
+            printf("Resultado: %d\n",MMC(A, B));
         }
         if (contador == 10)
         {   
@@ -113,14 +112,19 @@ int main() {
             scanf("%lf", &a);
             printf("Digite o segundo numero: ");
             scanf("%lf", &b);
-           // printf("Resultado: %.5lf\n",EQUACAO(a, b));
+            printf("Digite o terceiro numero: ");
+            scanf("%lf", &c);
+            if (a == 0)
+            {
+                printf("Nao e uma equacao de segundo grau");
+            }else{
+                EQUACAO(a, b, c);
+
+            }            
         }            
-
     } while (contador != 0);
-
     return 0;
 }
-
 double SOMA(double a, double b){
     double RESULTADO = 0.0;
     RESULTADO = a + b;
@@ -174,29 +178,31 @@ int FATORIAL(int a){
     }
     return (RESULTADO);
 }
-
 int MDC(int a, int b){
     int RESULTADO = 0;
-    if (a > b)
-    {
-        while((a % b) > 0){
+    
+        while(b != 0){
         RESULTADO = a % b;
         a = b;
-        b = RESULTADO;//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        b = RESULTADO;
         }
-    }
-    
-    
+    RESULTADO = a;
     return (RESULTADO);
-}/*
-int MMC(int a, int b){
+}
+int MMC(int a, int b) {
+    int MDCC = MDC(a, b);
     int RESULTADO = 0;
-    arrumaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-    return (RESULTADO);
+    RESULTADO = (a * b) / MDCC;
+    return (RESULTADO); 
 }
-int EQUACAO(double a, double b){
-    int double = 0.0;
-    arrumaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-    return (RESULTADO);
+double EQUACAO(double a, double b, double c){
+    double RESULTADO = 0.0, RESULTADO2 = 0.0, DELTA = 0.0;
+    double RAIZZ = 0.0;
+    DELTA = (b * b) - (4 * a * c);
+    RAIZZ = RAIZ(DELTA);
+    RESULTADO = (-b + RAIZZ) / 2 * a;
+    RESULTADO2 = (-b - RAIZZ) / 2 * a;
+    printf("Resultados: %.5lf, %5lf\n", RESULTADO, RESULTADO2);
+    return 0;
 }
-*/
+
