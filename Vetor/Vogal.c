a4cad0c86f7ea76c8792703f1b499e25f307f8f3
LarissaVitto <larissareginacosta@gmail.com>
2024-09-04 22:05:48 -0300

    vogal e consoante

--- Vetor/Vogal.c
+++ Vetor/Vogal.c
@@ -2,16 +2,25 @@
 
 int main() {
     char palavra[50];  
-    int i = 0, qtd = 0;
+    int i = 0, qtd = 0, contador = 0;
     scanf("%s", palavra);
-   if(palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' || palavra[i] == 'o' || palavra[i] == 'u')
-   {
-    qtd++;
-   }
-   while (palavra[i] != '\0')
+    while (palavra[i] != '\0')
    {
     i++;
    }
+    while (contador < i)
+    {
+        contador++;
+        if(palavra[i] != '\0')
+   {
+           if(palavra[i] == 'a' || palavra[i] == 'e' || palavra[i] == 'i' || palavra[i] == 'o' || palavra[i] == 'u')
+        {
+            qtd++;
+        }
+   }
+    }
+    
+   
    printf("A quantidade de letras eh: %d e de vogais eh: %d", i, qtd);
     return 0;
 }
