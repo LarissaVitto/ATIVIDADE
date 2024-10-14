abbba0114b83d2b9f791522f84ec9fefc462bc05
Larissa Vitto <110945122+LarissaVitto@users.noreply.github.com>
2024-10-14 20:52:18 -0300

    Add files via upload

---
+++ Prova/Dois.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int M = 1, N = 1, SOMA = 0, MENOR, MAIOR;
+    while(M > 0 && N > 0){
+        scanf("%d %d", &M, &N);
+        if(M <= 0 || N <= 0){
+            return 0;
+        }
+        if(M < N){
+            MENOR = M;
+            MAIOR = N;
+        }else{
+            MENOR = N;
+            MAIOR = M;
+        }
+        for(int i = MENOR; i <= MAIOR; i++){
+            printf(" %d", i);
+            SOMA += i;
+        }
+        printf(" SOMA=%d\n", SOMA);
+    }
+    return 0;
+}
